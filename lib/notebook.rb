5adfca6d75362fa1d0f8b3f1347755157990604c
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-18 12:54:18 +0100

    Added method and unit test to allow a user to input a new note

--- lib/notebook.rb
+++ lib/notebook.rb
@@ -1,2 +1,15 @@
 class Notebook
+  attr_reader :notes
+
+  def initialize
+    @list_of_notes = []
+  end
+
+  def new_note
+    puts "What is the title of this note?"
+    @title = gets.chomp
+    puts "What is the body of your note?"
+    @body = gets.chomp
+    @list_of_notes.push({title: @title, body: @body})
+  end
 end
--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -1,3 +1,12 @@
 require 'notebook'
 describe Notebook do
+  subject(:notebook) {described_class.new}
+
+  describe '#new_note' do
+    it { is_expected.to respond_to(:new_note) }
+    it 'should let you add a new_note for your note' do
+      allow(notebook).to receive(:gets).and_return("Daniel")
+      expect(notebook.new_note).to be == [{:title=>"Daniel", :body=>"Daniel"}]
+    end
+  end
 end

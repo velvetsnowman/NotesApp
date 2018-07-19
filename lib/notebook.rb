1f85a42792a73022957c331bab0da2581640e720
Daniel den Hartog <daniel.mista@gmail.com>
2018-07-20 00:02:39 +0100

    added an interactive menu for users

--- lib/notebook.rb
+++ lib/notebook.rb
@@ -5,6 +5,35 @@ class Notebook
     @list_of_notes = []
   end
 
+  def notebook_menu
+    loop do
+      print_menu
+      process(STDIN.gets.chomp)
+    end
+  end
+
+  def print_menu
+    puts "1. Input a new note"
+    puts "2. Show me all my notes"
+    puts "3. Pick a note"
+    puts "9. Exit"
+  end
+
+  def process(selection)
+    case selection
+    when "1"
+      new_note
+    when "2"
+      view_titles
+    when "3"
+      pick_note
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+    end
+  end
+
   def new_note
     puts "What is the title of your new note?"
     @title = gets.chomp
@@ -26,7 +55,7 @@ class Notebook
     view_titles
     puts "What note would you like to see?"
     input = gets.chomp.to_i
-    return "#{@list_of_notes[input-1][:title]} - #{@list_of_notes[input-1][:body]}"
+    puts "#{@list_of_notes[input-1][:title]} - #{@list_of_notes[input-1][:body]}"
   end
 
   private
@@ -34,4 +63,5 @@ class Notebook
     title.empty?
   end
 end
-Notebook.new.new_note
+
+Notebook.new.notebook_menu
